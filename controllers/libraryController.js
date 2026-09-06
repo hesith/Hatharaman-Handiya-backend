016d4975fdee1de020a3eb1838dc7f1f1b9c4382
Hesith Silva <hesith.silva@gmail.com>
2026-09-06 13:54:13 +0530

    libraryController fixed

--- controllers/libraryController.js
+++ controllers/libraryController.js
@@ -10,21 +10,19 @@ export async function GetLibraryStories(req, res) {
         }
 
         const likedArr = await getDb().collection('likes').find({ userId: userId }).project({ _id: 0, storyId: 1 }).toArray();
-        const ratedArr = await getDb().collection('ratings').find({ userId: userId }).project({ _id: 0, storyId: 1, rate: 1 }).toArray();
 
-        const idSet = new Set([...likedArr.map((l) => l.storyId), ...ratedArr.map((r) => r.storyId)]);
-        const ids = Array.from(idSet);
+        const ids = likedArr.map((l) => l.storyId);
 
         if (ids.length === 0) {
             return res.json([]);
         }
 
+        const ratedArr = await getDb().collection('ratings').find({ userId: userId, storyId: { $in: ids } }).project({ _id: 0, storyId: 1, rate: 1 }).toArray();
+
         await getDb().collection('StoryCard').find({ _id: { $in: ids }, statusId: PostStatus.APPROVED }).sort({ timestamp: -1 }).limit(200).toArray()
             .then((stories) => {
                 for (let s = 0; s < stories.length; s++) {
-                    if (likedArr.find((l) => l.storyId == stories[s]._id)) {
-                        stories[s].liked = true;
-                    }
+                    stories[s].liked = true;
 
                     const rating = ratedArr.find((r) => r.storyId == stories[s]._id);
                     if (rating) {
